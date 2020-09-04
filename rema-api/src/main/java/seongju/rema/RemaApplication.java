95b8073f8878d5bddef55d578d818f527e8a66ad
ParkSeongju <parksj914@naver.com>
2020-09-04 18:12:58 +0900

    first commit

---
+++ rema-api/src/main/java/seongju/rema/RemaApplication.java
@@ -0,0 +1,13 @@
+package seongju.rema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemaApplication.class, args);
+    }
+
+}
---
+++ rema-api/src/test/java/seongju/rema/RemaApplicationTests.java
@@ -0,0 +1,13 @@
+package seongju.rema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
